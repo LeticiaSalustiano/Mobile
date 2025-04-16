39f7545472d4ef27f18bf1794c38e58f96f321a0
Leeh <leticiasalustiano07@gmail.com>
2025-04-16 11:45:11 -0300

    .

---
+++ AppTII02_Banco/src/firebaseConnection.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD2ruy3y3QmkZjIbHkWXt40s75WfeTixow",
+  authDomain: "bancotii02-d152b.firebaseapp.com",
+  projectId: "bancotii02-d152b",
+  storageBucket: "bancotii02-d152b.firebasestorage.app",
+  messagingSenderId: "772917332502",
+  appId: "1:772917332502:web:8f448b494f3e9fc4204337",
+  measurementId: "G-HZ9WR22X0N"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
