5ec38a2aad990a2678c85e0cd5dd006b0531bdde
Leeh <leticiasalustiano07@gmail.com>
2025-04-28 11:46:56 -0300

    .

--- TII_Banco/App.js
+++ TII_Banco/App.js
@@ -1,14 +1,15 @@
-import { useEffect, useState } from 'react';
+import { use, useEffect, useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, TextInput, FlatList, Alert } from 'react-native';
-
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from './src/firebaseConnection';
 import { signInWithEmailAndPassword } from 'firebase/auth';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function App() {
 
   const[email, setEmail] = useState("")
   const[senha, setSenha] = useState("")
+  const[hidePass, setHidePass] = useState(true)
 
   const[authUser, setAuthUser] = useState(null)
 
@@ -16,7 +17,6 @@ export default function App() {
     const user = await createUserWithEmailAndPassword(auth, email, senha)
     console.log(user);
   }
-
 
   //async function loginUser(){
   // const user = await signInWithEmailAndPassword(auth, email, senha)
@@ -39,28 +39,37 @@ export default function App() {
 
      {/*<FormUsers></FormUsers>*/}
 
-     <Text style={styles.txt}>Email: </Text>
-     <TextInput 
-     style={styles.login} 
-     placeholder='Digite seu email...' 
-     value={email}
-     onChangeText={(texto)=> setEmail(texto)}
+  <Text style={styles.txt}>Email: </Text>
+   <TextInput 
+    style={styles.login} 
+    placeholder='Digite seu email...' 
+    value={email}
+    onChangeText={(texto)=> setEmail(texto)}
 />
 
-     <Text style={styles.txt}>Senha: </Text>
-     <TextInput 
-     style={styles.login} 
-     placeholder='Digite sua senha...' 
-     value={senha}
-     onChangeText={(texto)=> setSenha(texto)}
-     secureTextEntry={true}
-     />
+<Text style={styles.txt}>Senha: </Text>
+<View style={styles.inputContainer}>
+  <TextInput 
+    style={styles.input} 
+    placeholder="Digite sua senha..." 
+    value={senha} 
+    onChangeText={(texto) => setSenha(texto)}
+    secureTextEntry={hidePass}
+  />
+  
+  <TouchableOpacity 
+    onPress={() => setHidePass(!hidePass)}
+    style={styles.iconContainer}>
+    <Ionicons name={hidePass ? 'eye' : 'eye-off'} size={24} color="black" />
+  </TouchableOpacity>
+</View>
 
      <TouchableOpacity 
      style={styles.btn} 
      onPress={criarUser}>
       <Text style={styles.txtBtn}>Criar conta</Text>
      </TouchableOpacity>
+
      <TouchableOpacity 
      style={styles.btn} 
      onPress={loginUser}>
@@ -83,17 +92,35 @@ const styles = StyleSheet.create({
   login: {
     borderWidth: 1,
     margin: 15,
+  }, 
+ inputContainer: {
+    width: '92%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#000',
+    marginTop: 15,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+    marginLeft: 15,
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    paddingHorizontal: 8,
+  },
+  iconContainer: {
+    padding: 4, 
   },
   btn: {
-     backgroundColor: '#000',
-     margin: 15,
-     padding: 8,
-     width: 200,
-     alignItems: 'center',
-     alignSelf: 'center',
+    backgroundColor: '#000',
+    margin: 15,
+    padding: 8,
+    width: 200,
+    alignItems: 'center',
+    alignSelf: 'center',
   },
   txtBtn: {
-     color: '#fff',
-     
-  },
+    color: '#fff',  
+  }, 
 })
